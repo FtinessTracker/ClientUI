152751fd0b3db5a6584600b11bbe0e0815aee2cd
Jeevan <bjeevanreddy45@gmail.com>
2026-04-15 22:09:18 -0400

    workout lib config

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@
  * Global API configuration
  * Change API_BASE_URL here to switch environments.
  */
-// export const API_BASE_URL = 'https://trainliv.onrender.com';
-export const API_BASE_URL = 'http://localhost:8080';
+ export const API_BASE_URL = 'https://trainliv.onrender.com';
+// export const API_BASE_URL = 'http://localhost:8080';
